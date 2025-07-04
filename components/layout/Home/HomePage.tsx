c47ad47448706438e2289552da5770eed07d2dda
devosorio <contato.devosorio@gmail.com>
2025-07-04 17:03:31 -0300

    feat: update color variables in globals.css for improved theme consistency; add LEMA_ALL_YEARS constant with yearly references and texts

--- components/layout/Home/HomePage.tsx
+++ components/layout/Home/HomePage.tsx
@@ -3,8 +3,23 @@ import clsx from "clsx";
 export function HomePage() {
   return (
     <section className="flex-grow h-full">
-      <div className={clsx("container")}>
-        <h1>Welcome to the Home Page</h1>
+      <div
+        className={clsx(
+          "w-screen h-96 bg-red-900",
+          "flex items-center justify-center"
+        )}
+      >
+        <div className="container">
+          <h1
+            className={clsx(
+              "text-white text-4xl font-bold whitespace-pre-wrap text-center"
+            )}
+          >
+            {
+              'Lema 2025\n"Huid, salvad vuestra vida, y sed como retama en el desierto." - Jeremías 48:6'
+            }
+          </h1>
+        </div>
       </div>
     </section>
   );
---
+++ const/lemas-by-year.ts
@@ -0,0 +1,159 @@
+type LemaYearsType = {
+  year: number;
+  reference: string;
+  brazilianText: string;
+  reinaValeraText: string;
+};
+
+export const LEMA_ALL_YEARS: LemaYearsType[] = [
+  {
+    year: 2025,
+    reference: "Jer. 48:6",
+    brazilianText:
+      "Fugi, salvai a vossa vida e sereis como a tamareira no deserto.",
+    reinaValeraText:
+      "Huid, salvad vuestra vida, y sed como la retama en el desierto.",
+  },
+  {
+    year: 2024,
+    reference: "Apoc. 3:13",
+    brazilianText: "Quem tem ouvidos, ouça, o que o Espírito diz as Igrejas",
+    reinaValeraText:
+      "El que tiene oído, oiga lo que el Espíritu dice a las iglesias.",
+  },
+  {
+    year: 2023,
+    reference: "I Pe 1:5",
+    brazilianText:
+      "que, mediante a fé, estais guardados na virtude de Deus, para a salvação já prestes para se revelar no último tempo",
+    reinaValeraText:
+      "que sois guardados por el poder de Dios mediante la fe, para alcanzar la salvación que está preparada para ser manifestada en el tiempo postrero.",
+  },
+  {
+    year: 2022,
+    reference: "I Tim. 3:9",
+    brazilianText: "Guardando o mistério da fé...",
+    reinaValeraText: "que guarden el misterio de la fe...",
+  },
+  {
+    year: 2021,
+    reference: "Sal. 91:15",
+    brazilianText:
+      "Ele me invocará, e eu lhe responderei; estarei com ele na angústia; livrá-lo-ei e o glorificarei.",
+    reinaValeraText:
+      "Me invocará, y yo le responderé; Con él estaré yo en la angustia; Lo libraré y le glorificaré.",
+  },
+  {
+    year: 2007,
+    reference: "Is 41:13",
+    brazilianText:
+      "Porque eu, o Senhor teu Deus, te tomo pela tua mão direita, e te digo: Não temas, que eu te ajudo.",
+    reinaValeraText:
+      "Porque yo Jehová soy tu Dios, quien te sostiene de tu mano derecha, y te dice: No temas, yo te ayudo.",
+  },
+  {
+    year: 2006,
+    reference: "Det 30:19",
+    brazilianText:
+      "…Escolhe, pois, a vida, para que vivas, tu e a tua descendência.",
+    reinaValeraText:
+      "...Escoge, pues, la vida, para que vivas tú y tu descendencia.",
+  },
+  {
+    year: 2005,
+    reference: "Mat. 28:20",
+    brazilianText:
+      "Eis que estou convosco todos os dias até a consumação dos séculos.",
+    reinaValeraText:
+      "Y he aquí yo estoy con vosotros todos los días, hasta el fin del mundo.",
+  },
+  {
+    year: 2004,
+    reference: "Jos. 1:9",
+    brazilianText:
+      "Esforça-te, e tem bom ânimo; não pasmes, nem te espantes; porque o Senhor teu Deus é contigo, por onde quer que andares.",
+    reinaValeraText:
+      "Mira que te mando que te esfuerces y seas valiente; no temas ni desmayes, porque Jehová tu Dios estará contigo en dondequiera que vayas.",
+  },
+  {
+    year: 2003,
+    reference: "Rom. 8:37",
+    brazilianText:
+      "Mas em todas estas coisas somos mais que vencedores, por aquele que nos amou",
+    reinaValeraText:
+      "Antes, en todas estas cosas somos más que vencedores por medio de aquel que nos amó.",
+  },
+  {
+    year: 2002,
+    reference: "Sal. 91:1",
+    brazilianText:
+      "Aquele que habita no esconderijo do Altíssimo, a sombra do Onipotente descansará.",
+    reinaValeraText:
+      "El que habita al abrigo del Altísimo morará bajo la sombra del Omnipotente.",
+  },
+  {
+    year: 2001,
+    reference: "Apo. 21:5",
+    brazilianText: "…Eis que faço novas todas as coisas…",
+    reinaValeraText: "...He aquí yo hago nuevas todas las cosas.",
+  },
+  {
+    year: 2000,
+    reference: "Apo. 3:11",
+    brazilianText: "Guarda o que tens…",
+    reinaValeraText: "Retén lo que tienes...",
+  },
+  {
+    year: 1999,
+    reference: "Gen. 22:8",
+    brazilianText: "Deus proverá",
+    reinaValeraText: "Dios proveerá.",
+  },
+  {
+    year: 1998,
+    reference: "Sal. 23:1",
+    brazilianText: "Nada me faltará",
+    reinaValeraText: "Nada me faltará.",
+  },
+  {
+    year: 1997,
+    reference: "I Tess. 5:23",
+    brazilianText: "E o mesmo Deus de paz vos santifique em tudo…",
+    reinaValeraText: "Y el mismo Dios de paz os santifique por completo...",
+  },
+  {
+    year: 1996,
+    reference: "Mat. 24:42",
+    brazilianText:
+      "Vigiai, pois, porque não sabeis a que hora em que o filho do homem há de vir.",
+    reinaValeraText:
+      "Velad, pues, porque no sabéis a qué hora ha de venir vuestro Señor.",
+  },
+  {
+    year: 1995,
+    reference: "Rom. 8:31",
+    brazilianText: "Se Deus é por nós, quem será contra nós",
+    reinaValeraText: "Si Dios es por nosotros, ¿quién contra nosotros?",
+  },
+  {
+    year: 1994,
+    reference: "Nee. 8:10",
+    brazilianText:
+      "Enviai porções aos que não tem nada preparado para si; porque este dia é consagrado ao nosso Senhor…",
+    reinaValeraText:
+      "Enviad porciones a los que no tienen nada preparado; porque este día es santo a nuestro Señor...",
+  },
+  {
+    year: 1993,
+    reference: "Rom. 12:15",
+    brazilianText:
+      "Alegrai-vos com os que se alegram; chorai com os que choram.",
+    reinaValeraText: "Gozaos con los que se gozan; llorad con los que lloran.",
+  },
+  {
+    year: 1992,
+    reference: "Rom. 13:8",
+    brazilianText: "A ninguém devais coisa alguma, senão o amor…",
+    reinaValeraText: "No debáis a nadie nada, sino el amaros unos a otros...",
+  },
+];
