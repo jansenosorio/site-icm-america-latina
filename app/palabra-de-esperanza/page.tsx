f1bd253eece402ab70f9754d4c33d82065fdcfdb
devosorio <contato.devosorio@gmail.com>
2025-08-14 22:02:39 -0300

    feat: enhance PalabraDeEsperanzaPage with content and typography components

--- app/palabra-de-esperanza/page.tsx
+++ app/palabra-de-esperanza/page.tsx
@@ -1,7 +1,14 @@
+import { HeroSectionBlog } from "@/components/sections/HeroSectionBlog";
 import { PalabraDeEsperanzaPage } from "@/templates/Palabra-de-Esperanza/index";
 
 export default function PalabraEsperanzaPage() {
-    return (
-        <PalabraDeEsperanzaPage />
-    )
+  return (
+    <>
+      <HeroSectionBlog
+        title="Una palabra de esperanza"
+        description="Descubre mensajes de esperanza y fe que transforman vidas."
+      />
+      <PalabraDeEsperanzaPage />
+    </>
+  );
 }
--- templates/Palabra-de-Esperanza/PalabraDeEsperanca.tsx
+++ templates/Palabra-de-Esperanza/PalabraDeEsperanca.tsx
@@ -1,10 +1,36 @@
+import { Paragraph, ParagraphTitle } from "@/components/typography";
 import { clsx } from "clsx";
 
 export function PalabraDeEsperanzaPage() {
   return (
     <section className="flex-grow h-full">
       <div className={clsx("container-content mx-auto h-full px-4 pb-10")}>
-        {/* Aquí puedes agregar el contenido de la página */}
+        <ParagraphTitle>Nuestra Historia</ParagraphTitle>
+        <Paragraph>
+          La Iglesia Cristiana Maranata surgió en el seno de la comunidad
+          evangélica como resultado de un acontecimiento previsto para el tiempo
+          presente, como está escrito en el libro del profeta Joel 2:28, que
+          dice: Y después de esto derramaré mi Espíritu sobre toda carne, y
+          profetizarán vuestros hijos y vuestras hijas; vuestros ancianos
+          soñarán sueños, y vuestros jóvenes verán visiones.
+        </Paragraph>
+        <ParagraphTitle>El Fundador y el Fundamento</ParagraphTitle>
+        <Paragraph>
+          El fundador y el fundamento se identifican en la persona del Señor
+          Jesucristo. No hay, por tanto, énfasis en otro nombre o nombres, ya
+          que Su existencia es parte del plan profético de Dios para nuestros
+          días.
+        </Paragraph>
+        <Paragraph>
+          Sin embargo, la Iglesia Cristiana Maranatha, como institución
+          religiosa, nació en enero de 1968, en el municipio de Vila Velha,
+          Estado de Espírito Santo, Brasil, con el objetivo de adorar a Dios y
+          predicar el evangelio conforme a las Escrituras del Antiguo y Nuevo
+          Testamento como única regla de fe y práctica, así como concientizar la
+          formación espiritual y social del hombre, la educación cristiana,
+          promover obras de beneficencia y asistencia moral y educacional, sin
+          fines de lucro en territorio nacional y en el extranjero.
+        </Paragraph>
       </div>
     </section>
   );
